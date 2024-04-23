8ab4a539c708e419d7a8b39382c817cd0c3d90aa
Andrew Basem <andrewbassemsh@gmail.com>
2024-04-23 12:20:55 +0200

    add styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import "./App.css";
 
 function App() {
-
   const [message, setMessage] = useState(null);
   const handler = () => {
     fetch("https://distributed-system-project.onrender.com/helloWorldButton")
-    .then((response) => response.text())
-    .then((data) => {
-      setMessage(data);
-    });
-  }
+      .then((response) => response.text())
+      .then((data) => {
+        setMessage(data);
+      });
+  };
 
   return (
     <>
+      <GlobalStyles />
       <div>
         <button onClick={handler}>Click Me!</button>
         <p>{message}</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
