bbd9ff9f1874858552e0df6c13f7a77b98cd9dc9
Zeyad Waleed Amin <ziadwalid20035@gmail.com>
2024-04-23 21:50:58 +0200

    fixed image upload bug.

--- backend/utilities/image-upload.js
+++ backend/utilities/image-upload.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
-
+const { v4: uuidv4 } = require('uuid');
 exports.storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/images');
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date.toISOString() + '-' + file.originalname);
+    let extension = file.originalname.split('.').pop();
+    cb(null, uuidv4() + '.' + extension);
   },
 });
