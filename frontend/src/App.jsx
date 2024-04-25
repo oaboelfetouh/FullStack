0192a56914ac219cad8ca5229ec1a9cbf0ca3577
Ali Mohsen <ali_m_2001@hotmail.com>
2024-04-25 04:33:37 +0200

    remove hash router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import GlobalStyles from "./styles/GlobalStyles";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState } from 'react'
 import Base from "./routes/Base";
 import Home from "./routes/Home";
@@ -23,21 +23,21 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Base userType="customer"/>}>
             <Route index element={<Home userType="customer"/>} />
-            <Route path="/profile" element={<Profile userType="customer"/>}></Route>
-            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="profile" element={<Profile userType="customer"/>}></Route>
+            <Route path="cart" element={<Cart />}></Route>
           </Route>
           <Route path="/store" element={<Base userType="seller"/>}>
             <Route index element={<Home userType="seller"/>} />
-            <Route path="/store/profile" element={<Profile userType="seller"/>}></Route>
+            <Route path="profile" element={<Profile userType="seller"/>}></Route>
           </Route>
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   );
 }
