200f298d6e34e342a23c786e50ce7fc3438130c8
Andrew Gamal <andrewgamal7@gmail.com>
2024-04-25 13:24:49 +0200

    Added EditProduct endpoint

--- backend/controllers/seller.js
+++ backend/controllers/seller.js
@@ -119,3 +119,66 @@ exports.addProduct = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.editProduct = async (req,res,next) => {
+  const { userId } = req;
+  const { productId } = req.params;
+  const { name, description, price, availableQuantity, category } = req.body;
+
+  const errors = validationResult(req);
+  try{
+    if (!errors.isEmpty()) {
+      const error = new Error(
+        'Data Validation Failed.Please Enter Valid Data.'
+      );
+      error.status = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    let user;
+    user = await Seller.findById(userId);
+    // Checking Authorization
+    if (!user) {
+      const error = new Error('Not Authorized.');
+      error.status = 403;
+      throw error;
+    }
+    let product = await Product.findById(productId);
+    //Checking if this Product is valid
+    if (!product) {
+      const error = new Error('Product Not Found.');
+      error.status = 404;
+      throw error;
+    }
+    //Checking if this product is authorized to current seller
+    if (product.seller.toString() !== userId) {
+      const error = new Error('Not Authorized.');
+      error.status = 403;
+      throw error;
+    }
+    //checking that ImageURL field is not empty
+    if (req.file != undefined) {
+      const imageUrl = req.file.path;
+      product.imageUrl = imageUrl;
+    }
+    
+    //assignning req body to Product attribute
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    product.availableQuantity = availableQuantity;
+    product.category = category;
+    
+    const editedproduct = await product.save();
+    res.status(200).json({
+      message: 'Product Updated Succesfuly',
+      product: { ...editedproduct._doc},
+    });
+
+  }catch(err){
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
+};
