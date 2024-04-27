355eb862301ae9fe5b80331e2841e6d8e68dd657
Ahmed Ayman 2000128 <aa2712374@gmail.com>
2024-04-27 03:11:38 +0300

    working on collection of cards

---
+++ frontend/src/Components/ChoiceBox.jsx
@@ -0,0 +1,11 @@
+import "./ChoiceBox.css"
+export const ChoiceBox = () =>{
+    return (
+        <>
+            <select className="choiceBox">
+                <option value="price">price</option>
+                <option value="Name">Name</option>
+            </select>
+        </>
+    );
+}
