56f3c71f90e598a67a9270a8270f082a258b222f
neesaaa <131257593+neesaaa@users.noreply.github.com>
2024-04-28 18:24:31 -0400

    OrderItem component

---
+++ frontend/src/Components/OrderItem.jsx
@@ -0,0 +1,22 @@
+import "./OrderItem.css"
+
+import "./OrderItem.css"
+
+export default function OrderItem(props) {
+
+
+    return(
+        <>
+        <div className="container">
+      <div className="image">
+        <img src={props.OrderItem_image} alt="Image" />
+      </div>
+      <div className="header">
+        <h1>{props.OrderItem_name}</h1>
+        <h2>{props.OrderItem_price}</h2>
+        <h3>Quantity: {props.OrderItem_quantity}</h3>
+      </div>
+    </div>
+        </>
+    );
+}
